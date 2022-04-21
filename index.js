b26b36ca4077c5cf219fff0a32c0d170fbd75d29
David Hernandez <HL_David@outlook.com>
2022-04-21 12:27:16 -0500

    ADD: Class missionComander and index File

---
+++ index.js
@@ -0,0 +1,9 @@
+const MissionCommander = require('./app/missionComannder');
+
+const carlo = new MissionCommander("Carlo");
+const david = new MissionCommander("David");
+const natalia = new MissionCommander("Natalia");
+
+console.log(carlo.name);
+console.log(david.name);
+console.log(natalia.name);
